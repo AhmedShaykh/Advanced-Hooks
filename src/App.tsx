c37db01f89397300865eaf8cb12e74b071fa8ecd
AhmedShaykh <ahmed.shaykhwork@gmail.com>
2022-09-16 17:53:59 +0500

    Git Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
+import { useReducer } from 'react';
 import './App.css';
 import InterfaceProps from './Components/InterfaceProps';
 import Props from './Components/Props';
+import { initialState, Reducer } from './Components/Reducer';
 import Todo from './Components/Todo';
+import User from './Components/User';
 
 function App() {
+
+  const [state, dispatch] = useReducer(Reducer, initialState);
+
   return (
     <div className="App-header">
       <Props ts="TypeScript" />
       <Props>
         React TypeScript
       </Props>
-      
       <InterfaceProps lang=".JS" data="Next" />
       <Todo />
+      <User />
     </div>
   );
 }
