9d19ae0c884a725b7b84bc91e0f581cee684627e
AhmedShaykh <ahmed.shaykhwork@gmail.com>
2022-09-16 19:59:21 +0500

    Git Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
-import { useReducer } from 'react';
+import { createContext, useReducer } from 'react';
 import './App.css';
 import InterfaceProps from './Components/InterfaceProps';
 import Props from './Components/Props';
-import { initialState, Reducer } from './Components/Reducer';
+import { actionType, initialState, Reducer } from './Components/Reducer';
 import Todo from './Components/Todo';
 import User from './Components/User';
+
+type contextType = {
+  state: typeof initialState,
+  dispatch: React.Dispatch<actionType>,
+}
+
+export const UserContext = createContext({} as contextType);
 
 function App() {
 
   const [state, dispatch] = useReducer(Reducer, initialState);
 
   return (
-    <div className="App-header">
-      <Props ts="TypeScript" />
-      <Props>
-        React TypeScript
-      </Props>
-      <InterfaceProps lang=".JS" data="Next" />
-      <Todo />
-      <User />
-    </div>
+    <UserContext.Provider value={{ state, dispatch }}>
+      <div className="App-header">
+        <Props ts="TypeScript" />
+        <Props>
+          React TypeScript
+        </Props>
+        <InterfaceProps lang=".JS" data="Next" />
+        <Todo />
+        <User />
+      </div>
+    </UserContext.Provider>
   );
 }
 
--- src/Components/User.tsx
+++ src/Components/User.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import { useContext } from 'react';
+import { UserContext } from '../App';
 
 const User = () => {
+
+    const { state, dispatch } = useContext(UserContext);
+
     return (
-        <div>
-            <h1>
-                Hello
-            </h1>
+        <div className="container">
+            <h1>Name: {state.name}</h1>
+            <h1>Age: {state.age}</h1>
+            <button onClick={() => {
+                dispatch({ type: "CHANGE_NAME", payload: "Ahmed Shaikh" })
+            }}>Update Name</button>
+            <button onClick={() => {
+                dispatch({ type: "UPDATE_AGE", payload: 22 })
+            }}>Update Age</button>
         </div>
     )
 }
