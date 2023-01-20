f0d2864877c1053ffe36a878632faedc64274428
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-01-20 22:53:46 +0500

    Git Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,23 @@
-import { createContext, useReducer } from 'react';
+import React from 'react';
 import './App.css';
-import InterfaceProps from './Components/InterfaceProps';
-import Props from './Components/Props';
-import { actionType, initialState, Reducer } from './Components/Reducer';
-import Todo from './Components/Todo';
-import User from './Components/User';
-
-type contextType = {
-  state: typeof initialState,
-  dispatch: React.Dispatch<actionType>,
-}
-
-export const UserContext = createContext({} as contextType);
 
 function App() {
-
-  const [state, dispatch] = useReducer(Reducer, initialState);
-
   return (
-    <UserContext.Provider value={{ state, dispatch }}>
-      <div className="App-header">
-        <Props ts="TypeScript" />
-        <Props>
-          React TypeScript
-        </Props>
-        <InterfaceProps lang=".JS" data="Next" />
-        <Todo />
-        <User />
-      </div>
-    </UserContext.Provider>
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
